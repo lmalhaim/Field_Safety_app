b426a661dd58e938a21b037e0cffab2d0b636c1a
lmalhaim <63025546+lmalhaim@users.noreply.github.com>
2021-10-03 00:15:42 -0500

    nav set up

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { addUser } from './Api/api';
 import IncidentList from './screens/IncidentList';
 import FirstScreen from "./screens/first_screen";
 
 
+const Tab = createBottomTabNavigator(); 
 export default function App() {
   addUser(); 
   return (
     <View style={styles.container}>
-      < FirstScreen />
-      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen tabBarIcon = {""} name="Equipments" component={FirstScreen} />
+          <Tab.Screen name="Incidents" component={IncidentList} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
@@ -21,7 +27,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
 });
--- screens/first_screen.js
+++ screens/first_screen.js
@@ -1,49 +1,14 @@
 import * as React from "react";
 import styles from "../assets/StyleSheet.js";
 import { Image, Text, TouchableOpacity, View } from "react-native";
-import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { Searchbar } from 'react-native-paper';
-import IncidentList from './IncidentList';
 
-function HomeScreen() {
-  return (
-    <View>
-      <Text style={styles.bigBold}>
-          {'INCIDENTS'}
-      </Text>
 
-      <Searchbar
-          placeholder="Search for Incidents"
-          // onChangeText={query => { this.setState({ firstQuery: query }); }}
-          // value={firstQuery}
-        />
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View>
-      <Text>Equipments!</Text>
-    </View>
-  );
-}
-
-const Tab = createMaterialBottomTabNavigator();
 
 
 export default function FirstScreen(props) {
   return (
     <View style={styles.container}>
-
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Incidents" component={IncidentList} />
-          <Tab.Screen name="Equipments" component={SettingsScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
+      <Text> here </Text>
     </View>
   );
 }
