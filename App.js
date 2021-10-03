33e1cecd7bd4064ffdd23d9d09ab4831213661c2
lmalhaim <63025546+lmalhaim@users.noreply.github.com>
2021-10-02 22:43:22 -0500

    mod

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 <<<<<<< HEAD
+
 import { addUser } from './Api/api';
 import IncidentList from './screens/IncidentList';
 =======
